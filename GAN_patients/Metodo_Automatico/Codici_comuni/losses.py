ee6acb20ca830727395355aa481a5dd0ec86db99
GIOMANU01 <gioelemanuguerra9@gmail.com>
2026-01-28 14:07:44 +0100

    Create losses.py

---
+++ GAN_patients/Metodo_Automatico/Codici_comuni/losses.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LogSpectralConvLoss(nn.Module):
+    """
+    log-Spectral convergence loss module.
+    """
+
+    def __init__(self):
+        super(LogSpectralConvLoss, self).__init__()
+
+    def forward(self, x_mag, y_mag):
+        return torch.norm((y_mag - x_mag), p="fro") / torch.norm(y_mag, p="fro")
+
+
+class LogSTFTMagnitudeLoss(torch.nn.Module):
+    """
+    Log STFT magnitude loss module.
+    see https://arxiv.org/pdf/1808.06719.pdf
+    """
+
+    def __init__(self):
+        super(LogSTFTMagnitudeLoss, self).__init__()
+
+    def forward(self, x_mag, y_mag):
+        x_mag = torch.clamp(x_mag, min=1e-6, max=1e3) # avoid underflows & overflows
+        y_mag = torch.clamp(y_mag, min=1e-6, max=1e3)
+        return F.l1_loss(torch.log(x_mag), torch.log(y_mag))
